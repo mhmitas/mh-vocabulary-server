dfe3e7897bc2f73865f907394f85001c25616fb6
Mh Mitas <mahfuzulmitas@gmail.com>
2024-08-13 21:24:42 +0600

    start deploy and connect to atlas

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ app.use(cookieParser())
 
 
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jt5df8u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-const uri = "mongodb://localhost:27017";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jt5df8u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// const uri = "mongodb://localhost:27017";
 
 const client = new MongoClient(uri, {
     serverApi: {
